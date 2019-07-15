0826a819ab6167963c97afe9889fd841a2baf85a
Purva <Purva.gujiri12@gmail.com>
2019-07-15 15:57:33 +0530

    added collapse and icon in sidebar

--- dashdesign-master/src/app/sidebar/sidebar.component.ts
+++ dashdesign-master/src/app/sidebar/sidebar.component.ts
@@ -12,6 +12,23 @@ export class SidebarComponent implements OnInit {
   @HostBinding('class') classes = 'col col-xl-2 col-lg-3';
   modalTitle="Search"
   ngOnInit() {
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
   }
 
   showModal() {
@@ -20,5 +37,7 @@ export class SidebarComponent implements OnInit {
   hideModal(newName: string) {
       this.showIt = false;
   }
+
+
 
 }
